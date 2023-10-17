32f443a25e857bb98bcf5b6e27dc9b060d3fed87
Remsely <prokopchukromain@mail.ru>
2023-10-17 20:10:26 +0300

    feat: добавить реализацию различных задач и операций над ними

---
+++ src/tasks/Task.java
@@ -0,0 +1,57 @@
+package tasks;
+
+import java.util.Objects;
+
+public class Task {
+    protected int id;
+    protected String name;
+    protected String description;
+    protected String status;
+
+    public Task(int id, String name, String description, String status) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && Objects.equals(name, task.name) && Objects.equals(description, task.description)
+                && Objects.equals(status, task.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, status);
+    }
+
+    @Override
+    public String toString() {
+        return "tasks.Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
